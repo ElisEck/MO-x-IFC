ee6bef5f97b4f038603dc7257ccb11e01a23caee
Elisabeth Eckstaedt <elisabeth.eckstaedt@eas.iis.fraunhofer.de>
2022-12-09 10:49:41 +0100

    update knowledge graphs of Modelica Libraries and Modelica Example

--- src/test/java/modelicatranscriptor/parser/ModelicaFileAntlrParserTest.java
+++ src/test/java/modelicatranscriptor/parser/ModelicaFileAntlrParserTest.java
@@ -34,13 +34,12 @@ public class ModelicaFileAntlrParserTest {
      * liest die Inhalte eines Ordners in eine interne ModelicaLibrary und schreibt diese anschließend als ttl wieder raus
      */
     public void convertModelicaLibraryToGraph(){
-//        convertModelicaLibraryToGraph("lvb", "LVB", "c:\\_DATEN\\Modelica\\_Modelle\\LVB\\");
-//        convertModelicaLibraryToGraph("fmi4bim", "FMI4BIM", "c:\\_DATEN\\Modelica\\_Modelle\\FMI4BIM\\");
-        convertModelicaLibraryToGraph("libeas", "LibEAS", "c:\\_DATEN\\Modelica\\_Libraries_intern\\LibEAS\\", "Modelica LibEAS ontology", "v3.2.0.1");
-        convertModelicaLibraryToGraph("aix", "AixLib", "c:\\_DATEN\\Modelica\\_Libraries_extern\\AixLib 1.0.0\\", "Modelica AixLib (v1.0.0) ontology", "v1.0.0.1");
-        convertModelicaLibraryToGraph("ex", "LBDCG_example", "c:\\_DATEN\\Modelica\\_Modelle\\LBDCG_example\\", "knowledge graph representing the Modelica LBDCG_example package","v1.0.0.1");
-        convertModelicaLibraryToGraph("mbl", "Buildings", "c:\\_DATEN\\Modelica\\_Libraries_extern\\Buildings 8.0.0\\", "Modelica Buildings Library (v8.0.0) ontology","v8.0.0.1");
-        convertModelicaLibraryToGraph("msl", "Modelica", "C:\\Program Files\\Dymola 2021\\Modelica\\Library\\Modelica 3.2.3\\", "Modelica Standard Library (v3.2.3) ontology", "v3.2.3.1");
+        convertModelicaLibraryToGraph("ex", "LBDCG_example", "c:\\_DATEN\\WORKSPACES\\IntelliJ\\mo-x-ifc\\src\\test\\resources\\C_HeatPumpPlant\\LBDCG_example\\", "knowledge graph representing the Modelica LBDCG_example package","v1.0.0-1.0.0");
+        convertModelicaLibraryToGraph("libeas", "LibEAS", "c:\\_DATEN\\Modelica\\_Libraries_intern\\LibEAS\\", "Modelica LibEAS (v3.3.0) ontology", "v3.3.0-1.0.0");
+        convertModelicaLibraryToGraph("aix", "AixLib", "c:\\_DATEN\\Modelica\\_Libraries_extern\\AixLib 1.0.0\\", "Modelica AixLib (v1.0.0) ontology", "v1.0.0-1.0.0");
+        convertModelicaLibraryToGraph("mbl", "Buildings", "c:\\_DATEN\\Modelica\\_Libraries_extern\\Buildings 8.0.0\\", "Modelica Buildings Library (v8.0.0) ontology","v8.0.0-1.0.0");
+        convertModelicaLibraryToGraph("bs", "BuildingSystems", "c:\\_DATEN\\Modelica\\_Libraries_extern\\BuildingSystems 2.0.0-beta\\", "Modelica BuildingSystems Library (v2.0.0) ontology","v2.0.0-1.0.0");
+        convertModelicaLibraryToGraph("msl", "Modelica", "C:\\Program Files\\Dymola 2021\\Modelica\\Library\\Modelica 3.2.3\\", "Modelica Standard Library (v3.2.3) ontology", "v3.2.3-1.0.0");
     }
 
     /**
@@ -50,10 +49,10 @@ public class ModelicaFileAntlrParserTest {
      * @param sourcePath Ordner der das Wurzel package.mo enthält z.B. C:\Program Files\Dymola 2021\Modelica\Library\Modelica 3.2.3\
      */
     public void convertModelicaLibraryToGraph(String prefix, String longName, String sourcePath, String ontologyTitle, String ontologyVersion) {
-        ModelicaLibrary ml = new ModelicaLibrary(prefix, prefix, Path.of(sourcePath), ontologyTitle, ontologyVersion);
+        ModelicaLibrary ml = new ModelicaLibrary(prefix, prefix, Path.of(sourcePath));
         SimpleDateFormat sdf3 = new SimpleDateFormat("yyyyMMdd_HHmm");
 //        ml.serializeAsTTL("C:\\_DATEN\\WORKSPACES\\IntelliJ\\mo-x-ifc\\src\\test\\java\\output\\" + prefix+"_"+sdf3.format(new Timestamp(System.currentTimeMillis()))+ "short.ttl", prefix, longName,"short");
-        ml.serializeAsTTL("C:\\_DATEN\\WORKSPACES\\IntelliJ\\mo-x-ifc\\src\\test\\java\\output\\" + prefix+"_"+sdf3.format(new Timestamp(System.currentTimeMillis()))+ "_fullclean.ttl", prefix, longName,"fullclean");
+        ml.serializeAsTTL("C:\\_DATEN\\WORKSPACES\\IntelliJ\\mo-x-ifc\\src\\test\\java\\output\\" + prefix+"_"+sdf3.format(new Timestamp(System.currentTimeMillis()))+ "_fullclean.ttl", prefix, longName,"fullclean", ontologyTitle, ontologyVersion);
 //        ml.serializeAsTTL("C:\\_DATEN\\WORKSPACES\\IntelliJ\\mo-x-ifc\\src\\test\\java\\output\\" + prefix+"_"+sdf3.format(new Timestamp(System.currentTimeMillis()))+ "_full.ttl", longName,"full");//lässt sich in Protege nicht öffnen wegen Syntaxproblemen
     }
 
